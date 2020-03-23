398b5a09ef5382f0e782c0504b3cf8070fa394db
Akash Badarinath <akash0905@gmail.com>
2020-03-23 14:44:01 +0530

    Changes Made

--- Backend/app.js
+++ Backend/app.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === 'development') {
 
 //4.Limit requests from same API
 const limiter = rateLimit({
-  max: 100,
+  max: 150,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour'
 });
